f82eabf24491a82fb6c10fc96da67f2fca27248e
user name <oksadwinabila@gmail.com>
2023-12-13 21:33:37 +0700

    Basal Methabolic Rate Calculator

---
+++ src/main/java/com/mycompany/bmr/BMR.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bmr;
+
+/**
+ *
+ * @author User
+ */
+public class BMR {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
